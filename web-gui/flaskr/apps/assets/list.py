75298f2526aa2b1eda8dd387d16a1512d68c3afc
Krzysztof Kapusta <krz.kapusta@gmail.com>
2021-09-29 22:28:09 +0200

    Allow filtering assets by label and support labels in header links

--- web-gui/flaskr/apps/assets/list.py
+++ web-gui/flaskr/apps/assets/list.py
@@ -4,25 +4,36 @@ from flaskr import db, header
 from flaskr.stooq import Stooq
 
 
+def _getPipeline(label = None):
+    pipeline = []
+
+    match = { 'trashed': { '$ne' : True } }
+    if label is not None:
+        match['labels'] = label
+
+    pipeline.append({'$match': match})
+    pipeline.append({'$project': {
+        '_id': 1,
+        'name': 1,
+        'ticker': 1,
+        'institution': 1,
+        'type': 1,
+        'category': 1,
+        'subcategory': 1,
+        'region': 1,
+        'quantity': 1,
+        'link': 1,
+        'labels': 1,
+        'finalQuantity': { "$last": "$operations.finalQuantity" }
+    }})
+
+    return pipeline
+
+
 def listAll():
     if request.method == 'GET':
-        pipeline = [
-            { "$match" : { 'trashed': { '$ne' : True } } },
-            { "$project" : {
-                '_id': 1,
-                'name': 1,
-                'ticker': 1,
-                'institution': 1,
-                'type': 1,
-                'category': 1,
-                'subcategory': 1,
-                'region': 1,
-                'quantity': 1,
-                'link': 1,
-                'finalQuantity': { "$last": "$operations.finalQuantity" }
-            }}
-        ]
-        assets = list(db.get_db().assets.aggregate(pipeline))
+        label = request.args.get('label')
+        assets = list(db.get_db().assets.aggregate(_getPipeline(label)))
 
         return render_template("list.html", assets=assets, header=header.data())
 
