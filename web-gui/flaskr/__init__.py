6ad69bfd0d240e27e9bb851cd6dc6898b6049325
Krzysztof Kapusta <krz.kapusta@gmail.com>
2021-12-03 12:18:53 +0100

    Session cookie must be strict

--- web-gui/flaskr/__init__.py
+++ web-gui/flaskr/__init__.py
@@ -13,7 +13,8 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_mapping(
-        SECRET_KEY='dev'
+        SECRET_KEY = 'dev',
+        SESSION_COOKIE_SAMESITE = "Strict"
     )
 
     if test_config is None:
