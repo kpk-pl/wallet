29644c0cd26d6a171508bcd3b61adb34c30e8e6c
Krzysztof Kapusta <krz.kapusta@gmail.com>
2021-12-31 16:53:17 +0100

    Strong typing in profits analyzer - part 2

--- web-gui/flaskr/analyzers/profits.py
+++ web-gui/flaskr/analyzers/profits.py
@@ -11,6 +11,31 @@ class RunningTotals:
     investment: float = 0
     provision: float = 0
 
+    def avgPrice(self):
+        return self.price / self.quantity if self.quantity else 0
+
+    def avgNetPrice(self):
+        return self.netPrice / self.quantity if self.quantity else 0
+
+    def avgProvision(self):
+        return self.provision / self.quantity if self.quantity else 0
+
+    def partPrice(self, quantity):
+        return self.price * quantity / self.quantity
+
+    def partNetPrice(self, quantity):
+        return self.netPrice * quantity / self.quantity
+
+    def partProvision(self, quantity):
+        return self.provision * quantity / self.quantity
+
+    def scaleTo(self, quantity):
+        self.price = self.price * quantity / self.quantity
+        self.netPrice = self.netPrice * quantity / self.quantity
+        self.provision = self.provision * quantity / self.quantity
+        self.investment = self.investment * quantity / self.quantity
+        self.quantity = quantity
+
 
 class Profits(object):
     def __init__(self, assetData):
@@ -25,12 +50,7 @@ class Profits(object):
         }
 
         self.totalProfits = self.data['_totalProfits']
-
         self.investmentStart = None
-        self.averagePrice = 0.0
-        self.averageNetPrice = 0.0
-        self.averageProvision = 0.0
-        self.currentQuantity = 0.0
 
     def __call__(self):
         if 'operations' in self.data:
@@ -47,16 +67,16 @@ class Profits(object):
                     raise NotImplementedError("Did not implement profits for operation type {}" % (operation['type']))
 
                 stats = operation['_stats']
-                stats['averagePrice'] = self.averagePrice
-                stats['averageNetPrice'] = self.averageNetPrice
-                stats['averageProvision'] = self.averageProvision
+                stats['averagePrice'] = self._running.avgPrice()
+                stats['averageNetPrice'] = self._running.avgNetPrice()
+                stats['averageProvision'] = self._running.avgProvision()
 
                 self.currentQuantity = operation['finalQuantity']
 
-        self.data['_averagePrice'] = self.averagePrice
-        self.data['_averageNetPrice'] = self.averageNetPrice
-        self.data['_averageProvision'] = self.averageProvision
-        self.data['_stillInvestedNetValue'] = self.averageNetPrice * self.currentQuantity
+        self.data['_averagePrice'] = self._running.avgPrice()
+        self.data['_averageNetPrice'] = self._running.avgNetPrice()
+        self.data['_averageProvision'] = self._running.avgProvision()
+        self.data['_stillInvestedNetValue'] = self._running.investment
 
         if self.investmentStart is not None:
             self.data['_holdingDays'] = (datetime.now() - self.investmentStart).days
@@ -64,15 +84,10 @@ class Profits(object):
         return self.data
 
     def _buy(self, operation):
-        finalQuantity = operation['finalQuantity']
         self._running.quantity = operation['finalQuantity']
-
-        self.averagePrice = (self.averagePrice * self.currentQuantity + operation['price']) / finalQuantity
         self._running.price += operation['price']
-        self.averageNetPrice = (self.averageNetPrice * self.currentQuantity + _operationNetValue(operation)) / finalQuantity
         self._running.netPrice += _operationNetValue(operation)
         self._running.investment += _operationNetValue(operation)
-        self.averageProvision = (self.averageProvision * self.currentQuantity + _valueOr(operation, 'provision', 0.0)) / finalQuantity
         self._running.provision += _valueOr(operation, 'provision', 0)
 
         if self.investmentStart is None:
@@ -80,12 +95,7 @@ class Profits(object):
 
     # a RECEIVE is essentially a BUY with a price 0
     def _receive(self, operation):
-        finalQuantity = operation['finalQuantity']
         self._running.quantity = operation['finalQuantity']
-
-        self.averagePrice = (self.averagePrice * self.currentQuantity) / finalQuantity
-        self.averageNetPrice = (self.averageNetPrice * self.currentQuantity) / finalQuantity
-        self.averageProvision = (self.averageProvision * self.currentQuantity + _valueOr(operation, 'provision', 0.0)) / finalQuantity
         self._running.provision += _valueOr(operation, 'provision', 0)
 
         if self.investmentStart is None:
@@ -98,23 +108,16 @@ class Profits(object):
         profits = operation['_stats']['profits']
 
         profits = {
-            'value': operation['price'] - self.averagePrice * quantity,
-            'netValue': _operationNetValue(operation) - self.averageNetPrice * quantity,
-            'provisions': _valueOr(operation, 'provision', 0.0) + self.averageProvision * quantity
+            'value': operation['price'] - self._running.partPrice(quantity),
+            'netValue': _operationNetValue(operation) - self._running.partNetPrice(quantity),
+            'provisions': _valueOr(operation, 'provision', 0.0) + self._running.partProvision(quantity)
         }
 
-        self._running.price = self._running.price * (self._running.quantity - quantity) / self._running.quantity
-        self._running.netPrice = self._running.netPrice * (self._running.quantity - quantity) / self._running.quantity
-        self._running.provision = self._running.provision * (self._running.quantity - quantity) / self._running.quantity
-        self._running.investment = self._running.investment * (self._running.quantity - quantity) / self._running.quantity
-        self._running.quantity = operation['finalQuantity']
+        self._running.scaleTo(operation['finalQuantity'])
 
         self.totalProfits['value'] += profits['value']
         self.totalProfits['netValue'] += profits['netValue']
         self.totalProfits['provisions'] += profits['provisions']
 
         if operation['finalQuantity'] == 0:
-            self.averagePrice = 0.0
-            self.averageNetPrice = 0.0
-            self.averageProvision = 0.0
             self.investmentStart = None
