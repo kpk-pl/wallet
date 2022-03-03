cd3f262a7cd1509ecf48a3d82861166ff5af0b72
Krzysztof Kapusta <krz.kapusta@gmail.com>
2022-03-03 17:12:10 +0100

    Results page operations breakdown uses datatables

--- web-gui/flaskr/apps/results/static/results/index.js
+++ web-gui/flaskr/apps/results/static/results/index.js
@@ -8,7 +8,7 @@ $(function(){
 
 $(function () {
   $.fn.dataTable.Buttons.defaults.dom.collection.className += " dropdown-menu-right";
-  let datatable = $('#resultsTable').DataTable({
+  let resultsDatatable = $('#resultsTable').DataTable({
     paging: false,
     searching: true,
     ordering: true,
@@ -24,10 +24,32 @@ $(function () {
     ],
   });
 
-  datatable.buttons().container().appendTo("#tableElements");
+  let breakdownDatatable = $('#breakdownTable').DataTable({
+    paging: false,
+    searching: true,
+    ordering: true,
+    order: [[0, "asc"]],
+    info: false,
+    autoWidth: false,
+    responsive: true,
+    buttons: [{
+      extend: "colvis", className: "btn-sm py-0"
+    }],
+    columnDefs: [
+      { type: "string", targets: [0, 1, 2, 3] },
+      { type: "num-fmt", targets: [4, 5, 6, 7, 8, 9] },
+      { visible: false, targets: [0] }
+    ],
+  });
 
-  $("#list-filter").on("keyup search input paste cut", function() {
-    datatable.search(this.value).draw();
+  resultsDatatable.buttons().container().appendTo("#resultsTableElements");
+  breakdownDatatable.buttons().container().appendTo("#breakdownTableElements");
+
+  $("#resultsTableFilter").on("keyup search input paste cut", function() {
+    resultsDatatable.search(this.value).draw();
+  });
+  $("#breakdownTableFilter").on("keyup search input paste cut", function() {
+    breakdownDatatable.search(this.value).draw();
   });
 
   $('.color-gain').each(styling.colorGain);
