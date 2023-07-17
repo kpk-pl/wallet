bebc827a7ef2be991bffabed67c250d504494cd4
Krzysztof Kapusta <krz.kapusta@gmail.com>
2023-07-17 13:10:50 +0200

    Display Value instead of Total Value in wallet by default

--- web-gui/flaskr/apps/wallet/static/wallet/wallet.js
+++ web-gui/flaskr/apps/wallet/static/wallet/wallet.js
@@ -19,7 +19,7 @@ $(function(){
       { type: "string", targets: [0, 1, 2, 3] },
       { type: "num-fmt", targets: [4, 5, 6, 7, 8, 9, 10, 11] },
       { orderable: false, targets: [] },
-      { visible: false, targets: [1, 3, 4, 6, 7, 11] },
+      { visible: false, targets: [1, 3, 4, 6, 8, 11] },
     ],
   });
 
