721bbf3699291818b9f5908599d21f74c8f10d68
Krzysztof Kapusta <krz.kapusta@gmail.com>
2023-07-17 13:20:43 +0200

    Fix sparkline calculation
    For assets that existed once, were sold out completely with some profit
    and then were bought back again, sparkline was incorrectly calculated.

--- web-gui/flaskr/apps/wallet/static/wallet/wallet_sparklines.js
+++ web-gui/flaskr/apps/wallet/static/wallet/wallet_sparklines.js
@@ -17,17 +17,18 @@ function updateSparklines(historicalData, months, getter) {
                  .map(parseFloat)
                  .map(
                    (v, i) =>
-                     (v > 0 || parseFloat(asset.profit[i + fromIdx]) > 0) ?
+                     (v != 0 || parseFloat(asset.profit[i + fromIdx]) != 0) ?
                      utils.float.normalize(v + parseFloat(asset.profit[i + fromIdx]) - parseFloat(asset.investedValue[i + fromIdx])) :
                      null
                  ), options
     );
 
-    const initialValue = parseFloat(asset.value[fromIdx]) + parseFloat(asset.profit[fromIdx]);
-    const finalValue = parseFloat(asset.value[asset.value.length-1]) + parseFloat(asset.profit[asset.value.length-1]);
+    const initialValue = parseFloat(asset.value[fromIdx]);
+    const initialValueWithProfit = initialValue + parseFloat(asset.profit[fromIdx]);
+    const finalValueWithProfit = parseFloat(asset.value[asset.value.length-1]) + parseFloat(asset.profit[asset.value.length-1]);
     const investmentDiff = parseFloat(asset.investedValue[asset.value.length-1]) - parseFloat(asset.investedValue[fromIdx]);
     element.find(".value").html(
-      initialValue != 0 ? (((finalValue - initialValue - investmentDiff) / initialValue * 100.0).toFixed(2) + "%") : ""
+      initialValue != 0 ? (((finalValueWithProfit - initialValueWithProfit - investmentDiff) / initialValueWithProfit * 100.0).toFixed(2) + "%") : ""
     ).each(styling.colorGain);
   }
 }
